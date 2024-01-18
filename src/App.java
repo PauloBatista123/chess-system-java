3fd7c4f53027676f0e47c92381915e3e073ba433
PauloBatista123 <paulobatista.sistemas@gmail.com>
2024-01-18 11:04:07 -0300

    Start to implement Board and Piece

--- src/App.java
+++ src/App.java
@@ -1,8 +1,9 @@
-import boardgame.Position;
+import boardgame.Board;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Position position =  new Position(3,5);
-        System.out.println(position);
+        Board board = new Board(8, 8);
+
+        
     }
 }
